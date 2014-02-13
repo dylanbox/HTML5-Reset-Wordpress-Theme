b15188f11e11f7ddd3f7e7ac46a214cabc6d92be
Dylan Box <dylan@wedgedetroit.com>
2014-02-13 00:09:04 -0500

    Reducing File Sizes and Minifying CSS

--- header.php
+++ header.php
@@ -70,7 +70,7 @@
 
 	<!-- This is an un-minified, complete version of Modernizr.
 		 Before you move to production, you should generate a custom build that only has the detects you need. -->
-	<script src="<?php echo get_template_directory_uri(); ?>/_/js/modernizr-2.6.2.dev.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/_/js/modernizr-2.7.1.custom.js"></script>
 
 	<!-- Application-specific meta tags -->
 	<?php if (true == of_get_option('meta_app_win_name')) {
